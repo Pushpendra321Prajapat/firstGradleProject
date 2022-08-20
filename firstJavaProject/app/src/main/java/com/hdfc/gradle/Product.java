e452e0491048dafb44fc113f97f2b6d5f7366719
Pushpendra Prajapat <110169382+Pushpendra321Prajapat@users.noreply.github.com>
2022-08-20 18:28:05 +0530

    Add files via upload

---
+++ firstJavaProject/app/src/main/java/com/hdfc/gradle/Product.java
@@ -0,0 +1,46 @@
+package com.hdfc.gradle;
+
+public class Product {
+
+	private String name;
+	private String description;
+	private double price;
+	
+	public Product(String name, String description, Double price) {
+		this.name = name;
+		this.description = description;
+		this.price = price;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getDescription() {
+	return description;
+}
+
+	public double getPrice() {
+	return price;
+}
+
+	public void setName(String name) {
+	this.name = name;
+}	
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public String toString() {
+		return name + "  " + description + "  " + price;
+	};
+	
+		
+	}
+	
+	
+
